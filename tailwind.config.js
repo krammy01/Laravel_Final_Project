a9383c3b389164902e563b6c20bdd9bbbc322d76
KKJOVS <natrovez0914@gmail.com>
2024-08-09 22:44:43 +0800

    added times new roman for the profile blade

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                times: ['Times New Roman', 'serif'], // Added Times New Roman font
             },
         },
     },
